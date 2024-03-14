83115c34a08a797cce6ad1fccf6d13f674b1609b
baeksu <kyungwoo5826@naver.com>
2024-03-14 22:43:45 +0800

    todo-list -ing

---
+++ 50_todolist/script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const todosUl = document.querySelector("#todos");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addTodo();
+});
+
+function addTodo() {
+    let todoText = input.value;
+
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        todoEl.value = todoText;
+        todosUl.appendChild();
+    }
+}
